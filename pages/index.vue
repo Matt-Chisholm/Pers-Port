6b38c2f3793025c4e1f466c4b6aefac0462596e2
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-12 10:30:49 -0700

    initial setup:

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+ <div
+        class="absolute text-black text-center"
+        style="top: 50%; transform: translate(-50%, -50%); left: 50%;"
+      >
+        <h1 class="font-space-mono text-sm uppercase tracking-wide">
+          Matt Chisholm
+        </h1>
+        <p class="font-exo text-4xl">
+          Full Stack Web Developer
+        </p>
+        <a
+          href="https://github.com/Matt-Chisholm"
+          class="border px-4 py-2 rounded-lg text-sm font-space-mono uppercase mt-8 hover:bg-white hover:text-gray-800 inline-block"
+        >
+          Github
+        </a>
+      </div>
+</template>
+
+<script>
+export default {}
+</script>
